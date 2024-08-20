96bc1e5b1883d076c9772f0b4a9e2d95ec324743
MaganaTony <maganajarm@gmail.com>
2024-08-20 15:38:23 -0600

    Added footer

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import Navbar from './components/Navbar'
 import HeroSection from './components/HeroSection'
 import CardsSection from './components/CardsSection'
 import SponsorsSection from './components/SponsorsSection'
+import Footer from './components/Footer'
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <HeroSection />
       <CardsSection />
       <SponsorsSection />
+      <Footer />
     </>
   )
 }
---
+++ src/components/Footer.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Footer() {
+    return (
+        <footer className="flex flex-col items-center mt-10 mb-9 gap-1">
+        <p className="text-white/70 font-semibold">Released under the MIT License. (10611029)</p>
+        <p className="text-white/70 font-semibold">Copyright © 2019-present Evan You & Vite Contributors</p>
+        </footer>
+    );
+}
