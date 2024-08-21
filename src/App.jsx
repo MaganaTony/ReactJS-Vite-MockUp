64eb9029f9817efa30bb98a43853d076b93d3dff
MaganaTony <maganajarm@gmail.com>
2024-08-20 19:58:43 -0600

    Changed to send cards by props to comp CardsSection

--- src/App.jsx
+++ src/App.jsx
@@ -5,11 +5,44 @@ import SponsorsSection from './components/SponsorsSection'
 import Footer from './components/Footer'
 
 function App() {
+  const cards = [
+    {
+        icon: "💡",
+        title: "Instant Server Start",
+        description: "On demand file serving over native ESM, no bundling required!",
+    },
+    {
+        icon: "⚡",
+        title: "Lightning Fast HMR",
+        description: "Hot Module Replacement (HMR) that stays fast regardless of app size.",
+    },
+    {
+        icon: "🛠️",
+        title: "Rich Features",
+        description: "Out-of-the-box support for TypeScript, JSX, CSS and more.",
+    },
+    {
+        icon: "📦",
+        title: "Optimized Build",
+        description: "Pre-configured Rollup build with multi-page and library mode support.",
+    },
+    {
+        icon: "🔩",
+        title: "Universal Plugins",
+        description: "Rollup-superset plugin interface shared between dev and build.",
+    },
+    {
+        icon: "🔑",
+        title: "Fully Typed APIs",
+        description: "Flexible programmatic APIs with full TypeScript typing.",
+    },
+];
+
   return (
     <>
       <Navbar />
       <HeroSection />
-      <CardsSection />
+      <CardsSection cards={cards}/>
       <SponsorsSection />
       <Footer />
     </>
--- src/components/CardsSection.jsx
+++ src/components/CardsSection.jsx
@@ -1,46 +1,14 @@
 import React from "react";
 
-export default function CardsSection() {
-    const cards = [
-        {
-            icon: "💡",
-            title: "Instant Server Start",
-            description: "On demand file serving over native ESM, no bundling required!",
-        },
-        {
-            icon: "⚡",
-            title: "Lightning Fast HMR",
-            description: "Hot Module Replacement (HMR) that stays fast regardless of app size.",
-        },
-        {
-            icon: "🛠️",
-            title: "Rich Features",
-            description: "Out-of-the-box support for TypeScript, JSX, CSS and more.",
-        },
-        {
-            icon: "📦",
-            title: "Optimized Build",
-            description: "Pre-configured Rollup build with multi-page and library mode support.",
-        },
-        {
-            icon: "🔩",
-            title: "Universal Plugins",
-            description: "Rollup-superset plugin interface shared between dev and build.",
-        },
-        {
-            icon: "🔑",
-            title: "Fully Typed APIs",
-            description: "Flexible programmatic APIs with full TypeScript typing.",
-        },
-    ];
+export default function CardsSection(props) {
 
     return (
         <>
             <div className="container mx-auto mt-24 px-4 sm:px-6 lg:px-8">
                 <div className="grid gap-6 grid-cols-1 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-3 place-items-center">
-                    {cards.map((card, index) => (
+                    {props.cards.map((card, index) => (
                         <div
-                            key={index}
+                            key={card.index}
                             className="bg-[#202127] rounded-xl w-[300px] h-auto flex flex-col p-4 overflow-hidden"
                         >
                             <div className="flex justify-center items-center w-16 h-16 bg-[#65758529] rounded-xl mb-3">
